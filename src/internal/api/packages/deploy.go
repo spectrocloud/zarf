2f9799eaf898c0f8da3fd4c7035faa23d6d8c246
Tom Runyon <tom@defenseunicorns.com>
2022-10-13 07:05:26 -0400

    add all the files

--- src/internal/api/packages/deploy.go
+++ src/internal/api/packages/deploy.go
@@ -7,7 +7,7 @@ import (
 	"github.com/defenseunicorns/zarf/src/config"
 	"github.com/defenseunicorns/zarf/src/internal/api/common"
 	"github.com/defenseunicorns/zarf/src/internal/message"
-	"github.com/defenseunicorns/zarf/src/internal/packager"
+	"github.com/defenseunicorns/zarf/src/packager"
 	"github.com/defenseunicorns/zarf/src/types"
 )
 
